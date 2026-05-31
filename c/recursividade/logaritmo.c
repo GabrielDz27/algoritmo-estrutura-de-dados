ba5aea4e29446214225f348c1d9f067c5255f1a2
Gabriel Dziecinny Ferreira <gabidzferreira@gmail.com>
2026-05-31 18:25:28 -0300

    Create logaritmo.c

---
+++ c/recursividade/logaritmo.c
@@ -0,0 +1,22 @@
+// Programa: pisolog
+// Autor: GF
+//
+// Data: 2026-05-17
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int lg (int);
+
+
+int main (int numargs, char *arg[]) {
+	int B = strtol (arg[1], NULL, 10);
+	int K = strtol (arg[1], NULL, 10);
+	int N = 1;
+	printf (\"n		N lg(N)\n\n");
+
+	for (int i'
+}
+
